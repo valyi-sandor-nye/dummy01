ca031fd476089bac894e091e2831688ee7983d3e
valyi-sandor-nye <urugvalyi@gmail.com>
2025-09-10 13:37:23 +0200

    részekre bontottam a kiírást, egyelőre angolul

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Szia, szép új világ!");
+        System.out.println(a()+" "+b());
+
+    }
+
+    private static String b() {return "World";
+    }
+
+    private static String a() { return "Hello,";
     }
 }
